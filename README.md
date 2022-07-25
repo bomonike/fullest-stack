6ed5fb20633cc20b72bbc638146d6e8324a35936
wilsonmar <wilsonmar@gmail.com>
2022-07-24 21:04:58 -0600

    Add new

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # fullest-stack
 Components that go beyond a full stack
+
+This monorepo is intended to eventually contain code to create a complete production system with the features and processes necessary in today's hostile internet.
+
+<a target="_blank" href="https://res.cloudinary.com/dcajqrroq/image/upload/v1658717953/fullest-stack-flow-3138x1166_fdihf8.jpg"><img width="3138" height="1166" atl="fullest-stack-flow" src="https://res.cloudinary.com/dcajqrroq/image/upload/v1658717953/fullest-stack-flow-3138x1166_fdihf8.jpg"></a>
+
+We begin with the simplest set and gradually grow to that complete production system.
+
+1. The initial capability is a static site generated from GitHub Markdown by Jekyll running within GitHub, based on a template.
+
+1. The next level is using <strong>Python progams</strong> to generate GitHub Markdown code.
+   The caiq-html-gen.py program generates Markeown based on a csv file in the same folder.
+
+1. More sophisticated is Python code generating Markdown from <strong>Excel</strong> spreadsheets.
+
+1. Data maintenance utilities written in <strong>Bash</strong> are used to maintain secrets (using <strong>Vault</strong>), to clean and to reorganize data.
+
+1. Programs here are processed using CI/CD such as <strong>GitHub Actions</strong> (or CircleCI) processing <strong>Terraform</strong> to create resources in clouds, plus <strong>Ansible</strong> within servers.
+
+1. When static pages need authentication and authroization to accept payment of other user-specific interaction, an app is built using <strong>React.js</strong> to provide a UI front-end (web and mobile) with a <strong>back-end</strong> providing APIs.
+
+1. The API exposes makes use of a data store (database) to hold data specific to each user.
+
+1. An app data cache is used to handle more scale by minimizing rewriting of data.
+
+1. That database needs <strong>backups</strong> to ensure quick recovery from outages and complete disasters.
+
+1. That database stores data to be extracted and shwared as csv files.
+
+1. Logs and metrics are extracted for accumulation in a Time Series database.
+
+1. Policies are applied to identify (using OPA) violatations of rules defined.
+
+1. ML (Machine Language) is used to infer actions based on data accumulated over time.
---
+++ python/caiq-html-gen.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# caiq-html-gen.py
+# in prod-setup hashicups?
+
+import csv
+    # file-to-open='CAIQ4.0.1.csv'
+    # See https://courses.cs.washington.edu/courses/cse140/13wi/csv-parsing.html
+
+def print_caiq_line():
+  print("Hello from a function") 
+  
+with open('CAIQ4.0.1.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    caiq_rows_read = 0
+    caiq_rows_printed = 0
+    prev_first_qid_chars=""
+    caiq_categories = {
+    'A&A': 'Audit Assurance & Compliance',
+    'AIS': 'Application & Interface Security',
+    'BCR': 'Business Continuing Management & Operational Resilience',
+    'CCC': 'Change Control & Configuration Management',
+    'CEK': 'Cryptography, Encryption, and Key Management',
+    'DCS': 'Datacenter Security',
+    'DSP': 'Data Security & Privacy Lifecycle Management',
+    'GRC': 'Governance, Risk Management, and Compliance',
+    'HRS': 'Human Resources',
+    'IAM': 'Identity & Access Management',
+    'IPY': 'Interoperability & Portability',
+    'IVS': 'Infrastructure & Virtualization Security',
+    'LOG': 'Logging and Monitoring',
+    'SEF': 'Security Incident Management, E-Discovery, and Cloud Forensics',
+    'STA': 'Supply Chain Management, Transparancy, and Accountability',
+    'TVM': 'Threat and Vulnerability Management',
+    'UEM': 'Universal Endpoint Management'
+    }
+    
+    # User selections: TODO: choose in parameters:
+    print_annually=True
+    print_annually_only=True
+    print_answers=True
+
+    if print_annually == True :
+        print(f'*** Printing annually ')
+    else:
+       print(f'*** NOT Printing annually ')
+
+    if print_annually_only == True :
+        print(f'*** Printing annually_only ')
+    
+    # Print only if there is an answer: Print only if there is no answer:
+    if print_answers == True :
+       print(f'*** Printing answers ')
+    else:
+       print(f'*** NOT Printing answers ')
+
+    prev_title=""  # Def for next row.
+
+    for row in csv_reader:
+        if caiq_rows_read == 0:
+            print(f'*** Column names are {", ".join(row)}')
+            #  ID, _QID, _Title, _Question, _Answer_ID, _Answer
+            # WARNING: Retrieving first column (ID) causes an error.
+            caiq_rows_read += 1
+
+        # Rest of lines: Get first 3 characters of _QID:
+        first_qid_chars=row["_QID"][0:3]
+        
+        bool_print_caiq_line=True   # starting with all lines unless restricted.
+
+        if (row["_Question"].find('annual') != -1) :  # FOUND
+            # print(" annually found")
+            if print_annually == True :
+                bool_print_caiq_line=True
+        else:
+            if print_annually_only == True :
+                bool_print_caiq_line=False
+    
+        # See https://stackoverflow.com/questions/23866442/how-to-implement-efficient-filtering-logic-in-python
+
+        # Print only if there is an answer: Print only if there is no answer:
+        # if print_filter == "all" or len(row["_Answer"]) > 0 :
+        if bool_print_caiq_line == True :
+            caiq_rows_printed += 1
+
+            # TODO: Instead Lookup CategoryText from csv file?
+            # Lookup CategoryText from in-code table:
+            if first_qid_chars != prev_first_qid_chars :
+                Category_text = caiq_categories[first_qid_chars]  # not work
+                print(f'\r\n   <a name="{first_qid_chars}-"></a>\r\n\r\n   ### {first_qid_chars} = {Category_text} \r\n ')
+                prev_first_qid_chars = first_qid_chars
+                #   <a name="AIS-"></a>
+                #   ### AIS = Application & Interface Security  <- based on lookup of text for caiq-categories
+
+            if len(row["_Title"]) == 0 :
+                caiq_title=prev_title
+            else :
+                caiq_title=row["_Title"]
+            print(f'1. <a href="#{row["_QID"]}">{row["_QID"]}</a> - {caiq_title}<br /><br />\r\n   {row["_Question"]} ')
+
+            if print_answers == True :
+                # Show only if there is content - https://datagy.io/python-check-empty-string/
+                if len(row["_Answer"]) != 0  and print_answers == True :  # blank value
+                    if len(row["_Answer_ID"]) != 0 :  # blank value
+                       answer_text=row["_Answer_ID"] + " : "+ row["_Answer"]
+                    else :
+                       answer_text=": "+ row["_Answer"]
+                    print(f'   ANSWER {answer_text} \r\n')
+#                else :
+#                    print("   ________ \r\n")
+
+        prev_title=row["_Title"]  # for next row.
+
+        caiq_rows_read += 1
+    
+    print(f'*** {caiq_rows_read} rows read, -1 title row. {caiq_rows_printed} rows printed.')
