cc82d92aa3fc877b548d080d828f11d49d65cbb0
wilsonmar <wilsonmar@gmail.com>
2022-07-26 13:46:33 -0600

    Update

--- python/caiq-html-gen/caiq-html-gen.py
+++ python/caiq-html-gen/caiq-html-gen.py
@@ -6,16 +6,23 @@ import csv
     # See https://courses.cs.washington.edu/courses/cse140/13wi/csv-parsing.html
 
 # User selections: TODO: choose in parameters:
-file_to_open='CAIQ4.0.1.csv'
-
-# TODO: Output to file instead of STDOUT:
+bool_output_console=True
+bool_output_file=True
+file_to_open='CAIQ4.0.1.csv' 
 output_file_name="caiq-html-gen.txt"
+f = open(output_file_name, "a")
 
+print_categories=True
+print_questions=True
 print_annually=True
-print_annually_only=True
-print_answers=False
+print_annually_only=False   # ignore lines with annual in question
+print_answers=True
+print_answers_only=True     # ignore lines with no answers
+
+# Formatting user options:
+line_prefix="   "  # 3 chars
 category_format="bold"
-  
+
 with open(file_to_open, mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     caiq_rows_read = 0
@@ -55,6 +62,10 @@ with open(file_to_open, mode='r') as csv_file:
     else:
        print(f'*** NOT Printing answers ')
 
+    if bool_output_file == True :
+        if len(output_file_name) == 0 :
+            print("*** output_file_name not specified!")
+
     prev_title=""  # Def for next row.
 
     for row in csv_reader:
@@ -64,61 +75,78 @@ with open(file_to_open, mode='r') as csv_file:
             # WARNING: Retrieving first column (ID) causes an error.
             caiq_rows_read += 1
 
-        # Rest of lines: Get first 3 characters of _QID:
+        # Rest of lines: Get first 3 characters of _QID for Categories:
         first_qid_chars=row["_QID"][0:3]
         
         bool_print_caiq_line=True   # starting with all lines unless restricted.
 
-        if (row["_Question"].find('annual') != -1) :  # FOUND
-            # print(" annually found")
-            if print_annually == True :
-                bool_print_caiq_line=True
-        else:
+        # See https://stackoverflow.com/questions/23866442/how-to-implement-efficient-filtering-logic-in-python
+        if (row["_Question"].find('annual') != -1) :  # NOT FOUND
             if print_annually_only == True :
                 bool_print_caiq_line=False
     
-        # See https://stackoverflow.com/questions/23866442/how-to-implement-efficient-filtering-logic-in-python
-
+        if len(row["_Answer"]) == 0 and len(row["_Answer_ID"]) == 0 :  # Answer NOT present in line:
+            if print_answers_only == True :  
+                bool_print_caiq_line=False   # ignore lines with no answers
+    
         # Print only if there is an answer: Print only if there is no answer:
         # if print_filter == "all" or len(row["_Answer"]) > 0 :
         if bool_print_caiq_line == True :
             caiq_rows_printed += 1
+            prev_title=row["_Title"]  # for next row.
 
             # TODO: Instead Lookup CategoryText & CCM from csv file?
             # Lookup CategoryText from in-code table:
-            if first_qid_chars != prev_first_qid_chars :
-                Category_text = caiq_categories[first_qid_chars]  # not work
-                if category_format == "bold" :
-                    Category_display = "<strong>"+ first_qid_chars +" = "+ Category_text + "</strong>"
-                else:
-                    print(f'\r\n   <a name="{first_qid_chars}-"></a> ')
-                    Category_display = "### "+ first_qid_chars +" = "+ Category_text
-                print(f'\r\n   {Category_display} \r\n ')
-                
-                prev_first_qid_chars = first_qid_chars  # for next row.
-                #   <a name="AIS-"></a>
-                #   ### AIS = Application & Interface Security  <- based on lookup of text for caiq-categories
+            if print_categories == True :
+                if first_qid_chars != prev_first_qid_chars :
+                    category_text = caiq_categories[first_qid_chars]  # not work
+                    if category_format == "bold" :
+                        category_display = "<strong>"+ first_qid_chars +" = "+ category_text + "</strong>"
+                    else:
+                        category_display = "### "+ first_qid_chars +" = "+ category_text
+                    category_line="\r\n"+ line_prefix + category_display +"\r\n"
+                    if bool_output_console == True :
+                        print(category_line)
+                    if bool_output_file == True :
+                        f.write(category_line)
+
+                    prev_first_qid_chars=first_qid_chars  # for next row.
+                    #   <a name="AIS-"></a>
+                    #   ### AIS = Application & Interface Security  <- based on lookup of text for caiq-categories
 
             # If no title, get from previous row:
             if len(row["_Title"]) == 0 :
                 caiq_title=prev_title
             else :
                 caiq_title=row["_Title"]
-            print(f'1. <a href="#{row["_QID"]}">{row["_QID"]}</a> - {caiq_title}<br /><br />\r\n   {row["_Question"]} ')
+            # Mix of ' and " works?
+            title_line='1. <a href="#'+ row["_QID"] +">"+ row["_QID"] +"</a> - "+ caiq_title +"<br /><br />\r\n"
+               #print(f'1. <a href="#{row["_QID"]}">{row["_QID"]}</a> - {caiq_title}<br /><br />\r\n   {row["_Question"]} ')
+            if bool_output_console == True :
+                print(title_line)
+            if bool_output_file == True :
+                f.write(title_line)
+
+            if print_questions == True :
+                question_text=line_prefix+ row["_Question"] +"\r\n"
+                if bool_output_console == True :
+                    print(question_text)
+                if bool_output_file == True :
+                    f.write(question_text)
 
             if print_answers == True :
-                # Show only if there is content - https://datagy.io/python-check-empty-string/
-                if len(row["_Answer"]) != 0  and print_answers == True :  # blank value
-                    if len(row["_Answer_ID"]) != 0 :  # blank value
-                       answer_text=row["_Answer_ID"] + " : "+ row["_Answer"]
-                    else :
-                       answer_text=": "+ row["_Answer"]
-                    print(f'   ANSWER {answer_text} \r\n')
-#                else :
-#                    print("   ________ \r\n")
-
-        prev_title=row["_Title"]  # for next row.
+                if len(row["_Answer_ID"]) != 0 :  # blank value
+                    answer_text=row["_Answer_ID"] + " : "+ row["_Answer"]
+                else :
+                    answer_text=": "+ row["_Answer"]
+                answer_line=line_prefix+ "ANSWER "+ answer_text +"\r\n"
+                if bool_output_console == True :
+                    print(answer_line)
+                if bool_output_file == True :
+                    f.write(answer_line)
 
         caiq_rows_read += 1
     
     print(f'*** {caiq_rows_read} rows read, -1 title row. {caiq_rows_printed} rows printed.')
+    
+    f.close()
