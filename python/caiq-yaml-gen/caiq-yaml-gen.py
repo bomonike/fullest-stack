5d66ece1485ea9110fcd2df6c302c22f22000ee2
wilsonmar <wilsonmar@gmail.com>
2022-08-13 15:47:02 -0600

    Update

--- python/caiq-yaml-gen/caiq-yaml-gen.py
+++ python/caiq-yaml-gen/caiq-yaml-gen.py
@@ -2,16 +2,31 @@
 # caiq-yaml-gen.py in https://github.com/bomonike/fullest-stack/blob/main/python/caiq-yaml-gen/caiq-yaml-gen.py
 # by Wilson Mar and Kermit Vestal
 
-# This outputs a yaml-formatted file based on lines input from a csv file.
-# The output file will be copied to a GitHub repo's _posts folder for generation for display in a github.io site.
+# This reads a csv file (of CAIQ questions and answers) to create either lines or a HTML table
+# to Console and to a markdown output file. A yaml heading can be output, so that the output file
+# can be copied to a GitHub repo's _posts folder for generation for display in a github.io site.
 # There is a hierarchy in the inputs and outputs: Category, Title, Question, Answer.
-# Text is carries over from previous row so the assumed value is displayed if blank.
+# For looking up metrics text associated,
+# it builds an internal matrix by reading another CSV file.
+# Text is carried over from previous row so the assumed value is displayed if blank.
 # Output is filtered and formatted according to various boolean flags.
 
 import csv
 import os
 from datetime import datetime 
 from datetime import timezone
+
+import pandas as pd  # after pip install pandas
+# from csv import DictReader
+
+# Install pandas package:
+# See https://docs.python.org/3/library/subprocess.html#subprocess.check_call
+package="pandas"
+import subprocess
+import sys
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
 
 this_program_name="caiq-yaml-gen.py"
 
@@ -28,17 +43,25 @@ local_tzname = local_tz.tzname(local_now)
 # User selections: TODO: choose in parameters within a GUI:
 bool_output_console=True
 bool_output_file=True
-bool_output_table=True
+bool_output_table=False
 
-print_categories=True
+print_category_list=False
+bool_print_categories=True
+
+bool_print_metrics=True
+
 print_questions=True
 print_annually=True
-print_annually_only=False   # ignore lines with no annual in question
+print_annually_only=False   # True to ignore lines with no annual in question
 print_answers=True
-print_answers_only=True     # ignore lines with no answers
+print_answers_only=False     # True to ignore lines with no answers
+
+# This provides an example of how vendors of software can provide a customer-centric use of CAIQ.
+# > PROTIP: Phrase what your customer can say in a CAIQ after installing your product. That would save them the agony of drafting paragraphs for their auditors. That's the customer-centric approach.
 
 file_subject_text="for Consul users' auditors"
-file_to_open='CAIQ4.0.1.consul.csv' 
+caiq_file_to_open='CAIQ4.0.1.consul.csv'
+metrics_file_to_open='caiq-metrics-v1.csv'
 
 #output_file_date=str(local_dt)[0:10]
 output_file_date="2022-08-09"
@@ -85,15 +108,15 @@ if bool_output_file == True :
     f.write("date: \""+ output_file_date +"\"\r\n")
     f.write("file: \""+ output_file_prefix  +"\"\r\n")
     f.write("title: \"CAIQ (Consensus Assessment Initiative Questionnaire) " + file_subject_text +".\"\r\n")
-    f.write("excerpt: \""+ run_stats_line +" generated from file "+ file_to_open +" by "+ this_program_name +"\"\r\n")
+    f.write("excerpt: \""+ run_stats_line +" generated from file "+ caiq_file_to_open +" by "+ this_program_name +"\"\r\n")
     f.write("tags: [cloud, security, management, audit]" +"\r\n")
     f.write("---" +"\r\n")
 
-    f.write("\r\n")
-    f.write("<!-- At https://github.com/bomonike/fullest-stack/blob/main/python/caiq-yaml-gen/ -->\r\n")
-    f.write("\r\n")
+#    f.write("\r\n")
+#    f.write("<!-- At https://github.com/bomonike/fullest-stack/blob/main/python/caiq-yaml-gen/ -->\r\n")
+#    f.write("\r\n")
 
-    f.write("## Categories in the CAIQ\r\n")
+    f.write("## Categories in the CAIQ : \r\n")
     f.write("\r\n")
 
 # Output Category summary:
@@ -116,34 +139,49 @@ caiq_categories = {
     'TVM': 'Threat and Vulnerability Management',
     'UEM': 'Universal Endpoint Management'
     }
-for key, value in caiq_categories.items():
-    print_line="1. <a href=\"#"+ key +"-\"><tt>"+ key +"</tt></a> = "+ value
+    # TODO: UEM DEFINITION: Unified Endpoint Management (UEM) allows IT to manage, secure, and deploy corporate resources and applications on any device from a single console. UEM “unifies” legacy mobile device management (MDM) by incorporating IoT and other new device technologies.
+if print_category_list == True :
+    for key, value in caiq_categories.items():
+        print_line="1. <a href=\"#"+ key +"-\"><tt>"+ key +"</tt></a> = "+ value
+        if bool_output_console == True :
+            print(f'{print_line}')
+        if bool_output_file == True :
+            f.write(f'{print_line} \r\n')
     if bool_output_console == True :
-        print(f'{print_line}')
+        print("\r\n---------")
     if bool_output_file == True :
-        f.write(f'{print_line} \r\n')
-
-if bool_output_console == True :
-    print("\r\n---------")
-if bool_output_file == True :
-    f.write("\r\n<hr />\r\n")
+        f.write("\r\n<hr />\r\n")
 
 
-# Output yaml heading:
+if bool_print_metrics == True :
+   # pd=pandas, df=dataframe (table) instead of default encoding="utf-8" :
+   df = pd.read_csv(metrics_file_to_open, encoding="ISO-8859-1", sep = ",")
+   # print(df)  # display entire dataframe
+   df.set_index("_CCM_ID", inplace = True)
+   if bool_output_console == True :
+      print( "*** "+ str(len(df.index)) +" rows (excluding title row) in dataframe "+ metrics_file_to_open )
+
+   # TODO: Print list of CCM metrics described at https://cloudsecurityalliance.org/artifacts/metrics-and-measurements-for-the-csa-ccm/ and PDF downloaded from https://cloudsecurityalliance.org/download/artifacts/metrics-and-measurements-for-the-csa-ccm/
+
+
+# Output yaml heading and table heading:
 if bool_output_file == True :
     f.write("\r\n")
     f.write("## "+ run_stats_line +" in the CAIQ "+ file_subject_text +" (by Category)\r\n")
     f.write("\r\n")
     if bool_output_table == True :
         f.write('<table border="1" cellpadding="4" cellspacing="0">\r\n')
-        f.write('<tr valign="bottom"><th> CAIQ Item & Title </th><th> Question </th><th>Imp?</th><th> Answer </th></tr>\r\n')
+        f.write('<tr valign="bottom"><th> CAIQ Item & Title </th><th> Question </th><th> Answer </th></tr>\r\n')
         f.write("\r\n")
 
-with open(file_to_open, mode='r') as csv_file:
+
+with open(caiq_file_to_open, mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     caiq_rows_read = 0
     caiq_rows_printed = 0
     prev_first_qid_chars=""
+    metric_rows_printed = 0
+    prev_caiq_ccm_id=""
     
     category_lines_out=0
     prev_title=""  # Def for next row.
@@ -156,7 +194,8 @@ with open(file_to_open, mode='r') as csv_file:
             caiq_rows_read += 1
 
         # Rest of lines: Get first 3 characters of _QID for Categories:
-        first_qid_chars=row["_QID"][0:3]
+        first_qid_chars=row["_QID"][0:3]   # such as "A&A"
+        caiq_ccm_id=row["_QID"][0:6]       # such as "A&A-01" to lookup metric 
         
         bool_print_caiq_line=True   # starting with all lines unless restricted.
 
@@ -176,7 +215,7 @@ with open(file_to_open, mode='r') as csv_file:
 
             # TODO: Instead Lookup CategoryText & CCM from csv file?
             # Lookup CategoryText from in-code table:
-            if print_categories == True :
+            if bool_print_categories == True :
                 if first_qid_chars != prev_first_qid_chars :
 
                     # For GitHub Markdown weirdness:
@@ -186,21 +225,21 @@ with open(file_to_open, mode='r') as csv_file:
                         category_prefix=line_prefix
 
                     category_text = caiq_categories[first_qid_chars]  # not work
-                    category_line="<a name=\""+ first_qid_chars +"-\"></a>"
+                    category_line="<a name=\""+ first_qid_chars +"-\"></a>\r\n"
                     if bool_output_console == True :
-                        print(category_prefix+category_line+"\r\n")
+                        print("\r\n"+ category_prefix+category_line+"\r\n")
                     if bool_output_file == True :
                         if bool_output_table == True :
                             f.write('\r\n<tr valign="top"><td colspan="4">' + category_prefix+category_line )
                         else:
-                            f.write(category_prefix+category_line)
+                            f.write("\r\n"+ category_prefix+category_line)
 
                     if category_format == "bold" :
                         category_display = line_prefix+"<strong>"+ first_qid_chars +" = "+ category_text + "</strong>"
                     else:
                         category_display = category_prefix+"### "+ first_qid_chars +" = "+ category_text
 
-                    category_line="\r\n"+ category_display +"<br /><br />\r\n \r\n"
+                    category_line="\r\n"+ category_display +"\r\n \r\n"
                     if bool_output_console == True :
                         print(category_line)
                     if bool_output_file == True :
@@ -211,8 +250,30 @@ with open(file_to_open, mode='r') as csv_file:
 
                     category_lines_out += 1
                     prev_first_qid_chars=first_qid_chars  # for next row.
-                    #   <a name="AIS-"></a>
-                    #   ### AIS = Application & Interface Security  <- based on lookup of text for caiq-categories
+
+
+            if bool_print_metrics == True :
+                # TODO: Loop for multiple metrics per CAIQ item
+                caiq_ccm_id=row["_QID"][0:6]       # such as "A&A-01" to lookup metric 
+                metrics_line=""  # clear from previous.
+                if caiq_ccm_id != prev_caiq_ccm_id :
+                    try:
+                        # TODO: If CCM_ID is a Series in dataframe (not unique):
+
+                        metrics_line='CCM <a name="'+ df.loc[caiq_ccm_id,'_Metric_ID'] +'">'+ df.loc[caiq_ccm_id,'_Metric_ID'] +"</a> METRIC = "+ df.loc[caiq_ccm_id,'_Metric_Desc'] +" (SLO "+ str(df.loc[caiq_ccm_id,'_SLO']) +")"
+                        if bool_output_console == True :
+                            print(metrics_line +"\r\n")
+                        if bool_output_file == True :
+                            if bool_output_table == True :
+                                f.write(metrics_line)
+                            else:
+                                f.write("\r\n"+ metrics_line +"\r\n")
+                        metric_rows_printed += 1
+                    except:
+                        if bool_output_console == True :
+                            print(line_prefix+"*** No metric for "+ caiq_ccm_id )
+                    prev_caiq_ccm_id=caiq_ccm_id  # for next row.
+
 
             # If no title, get from previous row:
             if len(row["_Title"]) == 0 :
@@ -220,7 +281,7 @@ with open(file_to_open, mode='r') as csv_file:
             else :
                 caiq_title=row["_Title"]
             # Mix of ' and " works?
-            title_line='1. <a href="#'+ row["_QID"] +'"></a>'+ row["_QID"] +" - "+ caiq_title +"\r\n \r\n"
+            title_line="\r\n"+ str(caiq_rows_printed) +'. <a name="'+ row["_QID"] +'">'+ row["_QID"] +" - "+ caiq_title +"</a>\r\n"
             if bool_output_console == True :
                 print(title_line)
             if bool_output_file == True :
@@ -230,31 +291,29 @@ with open(file_to_open, mode='r') as csv_file:
                     f.write(title_line)
 
             if print_questions == True :
-                question_text=line_prefix+ row["_Question"] +"\r\n"
+                question_text=line_prefix+ row["_Question"]
                 if bool_output_console == True :
-                    print(question_text)
+                    print("\r\n"+question_text)
                 if bool_output_file == True :
                     if bool_output_table == True :
                         f.write("</td><td> " + row["_Question"] )
                     else:
-                        f.write(question_text)
+                        f.write("\r\n"+question_text)
 
-            if bool_output_table == True :
-                f.write("</td><td> Y ")
+            #if bool_output_table == True :
+            #    f.write("</td><td> Y ")
 
             if print_answers == True :
                 if len(row["_Answer_ID"]) != 0 :  # blank value
                     answer_text=row["_Answer_ID"] + " : "+ row["_Answer"]
-                else :
-                    answer_text=": "+ row["_Answer"]
-                answer_line=line_prefix+ "ANSWER "+ answer_text +"\r\n  \r\n"
-                if bool_output_console == True :
-                    print(answer_line)
-                if bool_output_file == True :
-                    if bool_output_table == True :
-                        f.write("</td><td> " + row["_Answer"] )
-                    else:
-                        f.write(answer_line)
+                    answer_line=line_prefix+ "ANSWER : "+ answer_text +"\r\n"
+                    if bool_output_console == True :
+                        print("\r\n"+answer_line)
+                    if bool_output_file == True :
+                        if bool_output_table == True :
+                            f.write("</td><td>" + row["_Answer"] )
+                        else:
+                            f.write("\r\n"+answer_line)
 
             if bool_output_table == True :
                 f.write('\r\n</td></tr>\r\n')
@@ -263,7 +322,7 @@ with open(file_to_open, mode='r') as csv_file:
         if len(row["_Title"]) > 0 :
            prev_title=row["_Title"]  # Save for next row if title is blank
 
-    last_stats_line=str(caiq_rows_read) +" rows read, "+ str(category_lines_out) +" categories, "+ str(caiq_rows_printed) +" rows printed."
+    last_stats_line=str(caiq_rows_read) +" CAIQ rows in, "+ str(category_lines_out) +" categories, "+ str(metric_rows_printed) +" CCM metrics. "+ str(caiq_rows_printed) +" questions+answers printed."
     if bool_output_console == True :
         print("*** "+ last_stats_line)
     if bool_output_file == True :
